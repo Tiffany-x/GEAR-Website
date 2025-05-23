5e31583177546df6dc8b88558c0eda436c27b425
tiffany <tiffany.priscillah@gmail.com>
2025-05-23 13:17:31 +0300

    Done with events, started on media

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.getElementsByClassName("CTA").addEventListener("click", function() {
-    var targetElement = document.getElementById("articles");
+function openDropdown() {
+    document.getElementById("region-dropdown").classList.toggle("show");
+}
 
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("topic-search");
+    filter = input.ariaValueMax.toUpperCase();
+    div = document.getElementById("region-dropdown");
 
-    window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth"
-    });
-})
+}

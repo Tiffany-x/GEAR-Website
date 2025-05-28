3294645270fda8a1eb8a9fead9cb46335279ff85
tiffany <tiffany.priscillah@gmail.com>
2025-05-28 21:05:03 +0300

    About expert page done

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ fetch('article.txt')
     document.getElementById('content').innerHTML = formattedText;
   })
   .catch(error => console.error('Error loading file:', error));
+
+fetch('Lora-Chase-bio.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Replace newlines with <br> and display
+    const formattedText = text.replace(/\n/g, '<br>');
+    document.getElementById('about').innerHTML = formattedText;
+  })
+  .catch(error => console.error('Error loading file:', error));

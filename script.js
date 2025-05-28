3e086130cafb97efb9df9de21fc1551fa9a335b2
tiffany <tiffany.priscillah@gmail.com>
2025-05-28 19:21:33 +0300

    read publications page finished

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function filterFunction() {
     div = document.getElementById("region-dropdown");
 
 }
+
+fetch('article.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Replace newlines with <br> and display
+    const formattedText = text.replace(/\n/g, '<br>');
+    document.getElementById('content').innerHTML = formattedText;
+  })
+  .catch(error => console.error('Error loading file:', error));

ed8e2afd7589fe2f20fe7996c6e85f5aef890c2f
tiffany <tiffany.priscillah@gmail.com>
2025-05-29 15:14:47 +0300

    Complete with dummy data. awaiting connection to database

--- script.js
+++ script.js
@@ -27,3 +27,30 @@ fetch('Lora-Chase-bio.txt')
     document.getElementById('about').innerHTML = formattedText;
   })
   .catch(error => console.error('Error loading file:', error));
+
+fetch('about-media.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Replace newlines with <br> and display
+    const formattedText = text.replace(/\n/g, '<br>');
+    document.getElementById('about-media').innerHTML = formattedText;
+  })
+  .catch(error => console.error('Error loading file:', error));
+
+fetch('about-event.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Replace newlines with <br> and display
+    const formattedText = text.replace(/\n/g, '<br>');
+    document.getElementById('about-event').innerHTML = formattedText;
+  })
+  .catch(error => console.error('Error loading file:', error));
+
+  fetch('about-event-speaker.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Replace newlines with <br> and display
+    const formattedText = text.replace(/\n/g, '<br>');
+    document.getElementById('about-event-speaker').innerHTML = formattedText;
+  })
+  .catch(error => console.error('Error loading file:', error));

8871be2e639c8856d0ea24e7bbf8068188d25cc3
tiffany <tiffany.priscillah@gmail.com>
2025-06-16 11:30:11 +0300

    removing deleted files from repo

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-#may also be 127.0.0.1
-#because the api is on the same server as the database
-$user = "nyxsolut_GEAR";
-$password = "FH5WY7xKVpJxeLJrKMJN";
-$db = "nyxsolut_GEAR";
-$conn = mysqli_connect($host, $user, $password, $db);
-?>
